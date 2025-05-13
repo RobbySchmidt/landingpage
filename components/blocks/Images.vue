92b8ba08b77b87eb6391b789c7a4c1bce4448a28
RobbySchmidt <168719558+RobbySchmidt@users.noreply.github.com>
2025-05-13 10:46:41 +0200

    Update Images.vue

--- components/blocks/Images.vue
+++ components/blocks/Images.vue
@@ -3,11 +3,11 @@
     <div class="container mx-auto pb-12 lg:pb-20">
       <div class="lg:w-8/12 mx-auto grid md:grid-cols-2 gap-6 text-gray-800 px-4">
         <NuxtLink 
-        v-if="block" 
-        v-for="image in block" 
-        :to="'/images/' + image.id"
-        :key="image.id"
-          class="space-y-4"
+          v-if="block" 
+          v-for="image in block" 
+          :to="'/images/' + image.id"
+          :key="image.id"
+          class="space-y-4 group overflow-hidden"
           >
           <h2
             v-if="image.title" 
@@ -18,13 +18,19 @@
           <img
             v-if="image.image" 
             :src="image.image"
+            class="lg:group-hover:scale-105 duration-500 ease-in-out"
             alt="" />
           <div
             v-if="image.description" 
             v-html="image.description.content"
             class="line-clamp-3">
           </div>
-          <span class="font-semibold">read more</span>
+          <span 
+            class="font-semibold">
+            read more
+            <MoveRight
+            class="inline-block duration-500 ease-in-out lg:group-hover:translate-x-1 w-5 h-5 ml-1"/>
+          </span>
         </NuxtLink>
       </div>
     </div>
@@ -32,6 +38,7 @@
 </template>
 
 <script setup>
+  import { MoveRight } from 'lucide-vue-next'
   const props = defineProps({
     block: String
   })
